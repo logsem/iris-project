c0a69ae8a542eb6bcc2666eef8b15d77e157ee7f
julesjacobs <julesjacobs@gmail.com>
2026-01-14 23:06:40 +0100

    Iris workshop 2026 website initial draft

---
+++ workshop-2026/schedule/generate.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+import sys, csv, string
+from datetime import date
+from itertools import groupby
+# usage: python3 generate.py speakers.csv >schedule.html
+
+def find_column(header, name):
+    '''Find the column named `name` and return its index'''
+    idx = 0
+    for column in header:
+        if column == name:
+            return idx
+        idx += 1
+    raise Exception(f"Column {name} not found")
+
+def mk_id(name):
+    return ''.join(
+        filter(lambda x: x in string.ascii_letters, name)
+    )
+
+class Talk:
+    name = None
+    affil = None
+    title = None
+    day = None
+    time = None
+    html = None
+
+# encoding='utf-8-sig' is required if the CSV file has a BOM prefix U+FEFF
+# encoding='utf-8' can be used otherwise
+f = csv.reader(open(sys.argv[1], newline='', encoding='utf-8-sig'), delimiter=';')
+header = next(f)
+
+name_col = find_column(header, 'Name Tag')
+affil_col = find_column(header, 'Affiliation')
+title_col = find_column(header, 'title')
+abstract_col = find_column(header, 'abstract')
+time_col = find_column(header, 'talk_time')
+slides_col = find_column(header, 'slides')
+html_col = find_column(header, 'raw_html')
+
+# output variable
+talks = []
+
+for row in f:
+    talk = Talk()
+    talk.name = row[name_col]
+    talk.affil = row[affil_col]
+    talk.title = row[title_col]
+    talk.abstract = row[abstract_col]
+    talk.slides = row[slides_col]
+    talk.html = row[html_col]
+    time = row[time_col]
+    if time:
+        (day, time) = time.split(' ')
+        talk.day = date.fromisoformat(day)
+        talk.time = time
+        talks.append(talk)
+
+talks.sort(key = lambda talk: talk.day)
+for (day, day_talks) in groupby(talks, lambda talk: talk.day):
+    print(f"<h3>{day:%A (%-d %B)}</h3>")
+    print("<ul>")
+    day_talks = sorted(day_talks, key = lambda talk: talk.time)
+    for talk in day_talks:
+        if talk.title:
+            id = mk_id(talk.name)
+            print(f"<li>{talk.time}: {talk.html} {talk.name} ({talk.affil})</a>: {talk.title}")
+            if talk.abstract:
+                print(f"<a href=\"#{id}\" data-bs-toggle=\"collapse\">[abstract]</a>")
+            if talk.slides:
+                url = talk.slides if talk.slides.startswith("https://") else "slides/"+talk.slides
+                print(f"<a href=\"{url}\">[slides]</a>")
+            # nested div makes the show/hide animation properly smooth
+            if talk.abstract:
+                print(f"<div id=\"{id}\" class=\"collapse\"><div class=\"abstract\">{talk.abstract}</div></div>")
+            print("</li>")
+        else:
+            print(f"<li>{talk.time}: {talk.html}</li>")
+    print("</ul>")
